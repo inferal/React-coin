1cd812fb9401557e82770c6d7c3a84827c83f273
inferal <pyurik44@gmail.com>
2018-04-18 20:45:34 +0300

    Props and typechecking with PropTypes

--- src/Components/list/List.js
+++ src/Components/list/List.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
-
-import './Table.css';
+import Table from './Table';
 
 export default class List extends Component {
   constructor() {
@@ -56,35 +55,10 @@ export default class List extends Component {
     }
 
     return (
-      <div className="Table-container">
-        <table className="Table">
-          <thead className="Table-head">
-            <tr>
-              <th>Cryptocurrency</th>
-              <th>Price</th>
-              <th>Market Cap</th>
-              <th>24H Change</th>
-            </tr>
-          </thead>
-          <tbody className="Table-body">
-            {currencies.map(currency => (
-              <tr key={currency.id}>
-                <td>
-                  <span className="Table-rank">{currency.rank}</span>
-                  {currency.name}
-                </td>
-                <td>
-                  <span className="Table-dollar">$ {currency.price}</span>
-                </td>
-                <td>
-                  <span className="Table-dollar">{currency.marketCap}</span>
-                </td>
-                <td>{this.renderChangePercent(currency.percentChange24h)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Table
+        currencies={currencies}
+        renderChangePercent={this.renderChangePercent}
+      />
     );
   }
 }
