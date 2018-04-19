ceb31f2b39c721bd2483822c0056f7a8dc44a305
inferal <pyurik44@gmail.com>
2018-04-19 10:55:41 +0300

    React Router

---
+++ src/Components/detail/Detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Detail.css';
+
+export default class Detail extends Component {
+    render(){
+        return(
+            <div>Detail</div>
+        )
+    }
+}
